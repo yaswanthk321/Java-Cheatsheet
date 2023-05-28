0c8597d1dc3db59f002702789b0ebf8e273115eb
Indracit S <indrajithij802@gmail.com>
2023-05-28 11:43:55 +0530

    how java works

---
+++ src/Lesson1.java
@@ -0,0 +1,20 @@
+// jvm should be installed in target machine
+// Java application is platform independent,
+// but jvm is platform dependent
+// jvm should run byte code only ,
+// so javac (java compiler) will compile .java file to bytecode(.class file)
+// in the bytecode java only search for only one file that contains main
+// method
+// eg : public static void main(String[] args)
+// jdk(java development kit) contains jre(java runtime environment ),
+// jre contains jvm(java virtual machine)  and libraries
+
+/**
+ * Lesson1
+ */
+public class Lesson1 {
+
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+    }
+}
